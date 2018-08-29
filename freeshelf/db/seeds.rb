69f7047bad8e36c7754f7ae670461d2685ee2180
Brandon Pitzen <bmpitzen@icloud.com>
2018-08-29 10:14:49 -0400

    New rails app with user and book models.

---
+++ freeshelf/db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Book.create(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    description: Faker::Hipster.paragraphs(1),
+    url: Faker::Internet.url
+  )
+end
+
+10.times do
+  User.create(user_name: Faker::Internet.username(5..8))
+end
